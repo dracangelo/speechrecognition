54cf2520afeb3d9ae8032185f9386389f5e1d90a
dracangelo <kevinmbuguaw@gmail.com>
2023-03-09 10:09:59 +0300

    implimented online search

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import speech_recognition as sr
+from time import ctime
+import webbrowser
 
 r = sr.Recognizer()
 
-def record_speech():
+def record_speech(ask=False):
     with sr.Microphone() as source:
+        if ask:
+            print(ask)
         audio = r.listen(source)
         voice_data = ''
         try:
             voice_data = r.recognize_google(audio)
-            print(voice_data)
         except sr.UnknownValueError:
             print("Unknown")
         except sr.RequestError:
             print("Request error")
         return voice_data
+    
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        print('autoaispeech')
+    if 'what time is it' in voice_data:
+        print(ctime())
+    if 'search' in voice_data:
+        search = record_speech("what do you want to search for?")
+        url = 'https://www.google.com/search?q=' + search
+        webbrowser.get().open(url)
+        print('here is what I found ' + search)
 
 print("Listening...")
+voice_data = record_speech()
+respond(voice_data)
