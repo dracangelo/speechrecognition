6e0526265114c34f833ef46c4aa5b644e13391cc
dracangelo <kevinmbuguaw@gmail.com>
2023-03-09 09:43:33 +0300

    added a mic instance

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import speech_recognition as sr
 
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Listening...")
+    audio = r.listen(source)
+    voice_data = r.recognize_google(audio)
+    print(voice_data)
