0ea754d7c2bd07efa2147861ccfc518ab190b933
dracangelo <kevinmbuguaw@gmail.com>
2023-03-09 09:54:33 +0300

    wrapped everything in a function

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print("Listening...")
-    audio = r.listen(source)
-    try:
-        voice_data = r.recognize_google(audio)
-        print(voice_data)
-    except sr.UnknownValueError:
-        print("Unknown")
-    except sr.RequestError:
-        print("Request error")
+def record_speech():
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)
+            print(voice_data)
+        except sr.UnknownValueError:
+            print("Unknown")
+        except sr.RequestError:
+            print("Request error")
+        return voice_data
+
+print("Listening...")
